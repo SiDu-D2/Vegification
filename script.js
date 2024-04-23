ae3ad3dce50369cada1bf042791ff814ec1caccd
Simon Duff <64978006+SiDu-D2@users.noreply.github.com>
2024-04-23 14:01:14 +0100

    initial draft of site.
    More subs and filter features needed to be turned on

---
+++ script.js
@@ -0,0 +1,22 @@
+const veganSubs = {
+    'milk': 'almond milk',
+    'butter': 'vegan butter',
+    'cheese': 'nutritional yeast',
+    'cream': 'coconut cream',
+    'eggs': 'mashed banana',
+    'honey': 'maple syrup',
+    'gelatin': 'agar agar',
+    'chicken': 'chicken substitute'
+};
+
+function convertRecipe() {
+    const inputText = document.getElementById('recipeInput').value;
+    let outputText = inputText;
+
+    Object.keys(veganSubs).forEach(nonVegan => {
+        const regex = new RegExp(nonVegan, 'gi');
+        outputText = outputText.replace(regex, veganSubs[nonVegan]);
+    });
+
+    document.getElementById('veganOutput').innerText = outputText;
+}
